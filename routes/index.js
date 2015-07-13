be908254f98b27a1c15c8ea058dd63b51d2840bf
chengxueyong <1361272604@qq.com>
2015-07-13 17:16:35 +0800

    improve login and register page

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('login', { title: '用户登录' });
 });
 
 router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Express' });
+  res.render('register', { title: '用户注册' });
 });
 
 router.get('/monitor', function(req, res, next) {
-  res.render('monitor', { title: 'Express' });
+  res.render('monitor', { title: '空气质量检测系统' });
 });
 
 module.exports = router;
